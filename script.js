35ebaedd7a5a8f8614e5ce620565aef22b8b4b8d
Xoumita <soumita2003d1@gmail.com>
2024-01-18 17:17:12 +0530

    complete

---
+++ script.js
@@ -0,0 +1,31 @@
+let yesBtn = document.querySelector("#yes")
+let NoBtn = document.querySelector("#no")
+let btnDiv = document.querySelector(".btn")
+let defaultImg = document.querySelector("#default")
+
+yesBtn.addEventListener("click", function(){
+    NoBtn.style.display = "none";
+    yesBtn.style.background = "transparent";
+    yesBtn.style.color = "black"
+    yesBtn.style.left = "43%"
+    yesBtn.innerHTML = "Let's Go!"
+    defaultImg.style.display = "none"
+
+})
+
+NoBtn.addEventListener("mouseover", function(){
+    moveBtnRandomly()
+})
+NoBtn.addEventListener("click", function(){
+    moveBtnRandomly()
+})
+function moveBtnRandomly() {
+    var windowWidth = window.innerWidth - NoBtn.offsetWidth;
+    var windowHeight = window.innerHeight - NoBtn.offsetHeight;
+
+    var randomX = Math.floor(Math.random() * windowWidth);
+    var randomY = Math.floor(Math.random() * windowHeight);
+
+    NoBtn.style.left = `${randomX}` + "px";
+    NoBtn.style.top = `${randomY}` + "px";
+}
